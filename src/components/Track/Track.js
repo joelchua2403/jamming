5dd0718a6d43e94e3cba95c8608908335f44d5a6
Joel Chua <joel.chua.mingyi@gmail.com>
2023-02-17 14:01:33 +0800

    add ability to pass state from App component to Playlist, SearchResults to Tracklist

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -2,16 +2,6 @@ import React from "react";
 import "./Track.css";
 
 class Track extends React.Component {
-    render() {
-        return (
-            <div className="Track">
-                <div className="Track-information">
-                    <h3>track name will go here</h3>
-                    <p>track artist will go here | track album will go here</p>
-                </div>
-            </div>
-        );
-    }
 
     renderAction() {
         if (this.props.isRemoval) {
@@ -21,6 +11,19 @@ class Track extends React.Component {
         }
 
 }
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
+                </div>
+                {this.renderAction()}
+            </div>
+        );
+    }
+
+   
 }
 
 export default Track;
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -6,9 +6,13 @@ class TrackList extends React.Component {
 render() {
 return (
     <div className="TrackList">
-    < Track />
-</div>
-);
+        {
+            this.props.tracks.map(track => {
+                return <Track track={track} key={track.id} />
+                })
+            }
+            </div>
+        )
     }
 }
 
